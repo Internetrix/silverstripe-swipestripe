f1016135df0f27526dcb8c270d1aaa64c47ca94b
Jason <jason-zz@users.noreply.github.com>
2014-12-08 11:24:37 +1100

    Update OrderForm.php
    Pass member details to payment gateway (tested for eway).

--- code/form/OrderForm.php
+++ code/form/OrderForm.php
@@ -321,7 +321,13 @@ class OrderForm extends Form {
 			$paymentData = array(
 				'Amount' => number_format($order->Total()->getAmount(), $precision, '.', ''),
 				'Currency' => $order->Total()->getCurrency(),
-				'Reference' => $order->ID
+				'Reference' => $order->ID,
+				'Customer' 	=> array(
+					'FirstName' 	=> $member->FirstName,
+					'LastName' 	=> $member->Surname,
+					'Phone' 	=> $member->Phone,
+					'Email' 	=> $member->Email
+				)
 			);
 			$paymentProcessor->payment->OrderID = $order->ID;
 			$paymentProcessor->payment->PaidByID = $member->ID;
